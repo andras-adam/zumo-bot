8303d2d25f0a69061ee4d1feb7b86f424176f959
Keijo Länsikunnas <keijo.lansikunnas@metropolia.fi>
2018-09-25 08:50:49 +0300

    Increased heap size

--- ZumoBot.cydsn/ZumoLibrary/FreeRTOSConfig.h
+++ ZumoBot.cydsn/ZumoLibrary/FreeRTOSConfig.h
@@ -47,7 +47,7 @@
 #define configCPU_CLOCK_HZ			( ( unsigned long ) BCLK__BUS_CLK__HZ )
 #define configTICK_RATE_HZ			( ( TickType_t ) 1000 )
 #define configMINIMAL_STACK_SIZE	( ( unsigned short ) 100 )
-#define configTOTAL_HEAP_SIZE		( ( size_t ) ( 32 * 1024 ) )
+#define configTOTAL_HEAP_SIZE		( ( size_t ) ( 36 * 1024 ) )
 #define configMAX_TASK_NAME_LEN		( 12 )
 #define configUSE_TRACE_FACILITY	1
 #define configUSE_STATS_FORMATTING_FUNCTIONS 1    
